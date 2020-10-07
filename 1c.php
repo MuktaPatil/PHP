8abdbb95d9e4ebabc758dd368789653b99dd45b4
MuktaPatil <72442967+MuktaPatil@users.noreply.github.com>
2020-10-07 18:18:24 +0530

    3 pracs added

---
+++ 1c.php
@@ -0,0 +1,17 @@
+<?php
+$a = 5;
+$b = 5;
+$c = $a + $b;
+	echo "Addition : ".$c;
+$d = $a - $b;
+	echo "<br>Subttraction : ".$d;
+$e = $a * $b;
+	echo "<br>Multiplication : ".$e;
+$f = $a / $b;
+	echo "<br>Division : ".$f;
+$g = $a % $b;
+	echo "<br>Modulus : ".$g;
+$h = $a**$b;
+	echo "<br>Powers : ".$h;
+
+?>
---
+++ circ.php
@@ -0,0 +1,9 @@
+<?php
+$r = 34;
+$a = 3.14 *$r*$r;
+$p = 2 * 3.14 *$r;
+echo "<h1> Circle </h1><br>";
+echo "Area:  $a <br> Perimeter: $p";
+
+
+?>
