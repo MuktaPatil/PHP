394523f17801a2c74a797f4e5da47b82fec13326
MuktaPatil <72442967+MuktaPatil@users.noreply.github.com>
2020-10-13 14:27:52 +0530

    IF and IF-ELSE conditional statements

---
+++ if.php
@@ -0,0 +1,9 @@
+<?php
+//IF Statement
+$x = 3;
+echo "$x <br>";
+if($x>0)
+	echo "Positive number";
+
+
+?>
---
+++ ifelse.php
@@ -0,0 +1,15 @@
+<?php
+//IF ELSE Statement
+
+$age = 23;
+echo "$age <br>";
+ if($age>=18)
+ 
+echo "Voter!!";
+
+
+else
+
+echo " Not a Voter";
+
+?>
