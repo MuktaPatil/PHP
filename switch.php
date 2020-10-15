e1e891ab7c7b928a77263e148efc077b7467a63b
MuktaPatil <72442967+MuktaPatil@users.noreply.github.com>
2020-10-15 18:32:02 +0530

    switch and volume program

---
+++ switch.php
@@ -0,0 +1,32 @@
+<?php
+echo "<h3>Switch Case</h3>";
+$x = 3;
+echo "$x <br>";
+
+switch($x)
+{
+case 1:
+	echo "Monday";
+	break;
+case 2:
+	echo "Tuesday";
+	break;
+case 3:
+	echo "Wednesday";
+	break;
+case 4:
+	echo "Thursday";
+	break;
+case 5:
+	echo "Friday";
+	break;
+case 6:
+	echo "Sunday";
+	break;
+case 7:
+	echo "Sunday";
+	break;
+default:
+	echo "Invalid....";
+}
+?>
---
+++ vol.php
@@ -0,0 +1,8 @@
+<?php
+$r = 1;
+$a = 4/3.0 * 3.14 * ($r**3);
+
+echo "Volume of Sphere: $a";
+
+
+?>
